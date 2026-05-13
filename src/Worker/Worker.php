6d1819bff8ecec169b558ca6da27e43d301c4ed6
Yan <minibaev2001@yandex.ru>
2026-05-13 23:44:49 +0400

    bug report for edmond

--- src/Worker/Worker.php
+++ src/Worker/Worker.php
@@ -70,8 +70,10 @@ final class Worker
 
         try {
             await($producer);
-        } catch (\Cancellation|\Async\ThreadChannelException) {
+        } catch (\Cancellation|\Async\ThreadChannelException $e) {
             error_log('[Thrun Worker] Producer \Cancellation or ThreadChannelException');
+            $cls = get_class($e);
+            error_log("{$cls} {$e->getMessage()} {$e->getTraceAsString()}");
         } catch (\Throwable $e) {
             error_log('[Thrun Worker] Producer coroutine error: '.$e::class.': '.$e->getMessage());
             if ($abnormalError === null) {
@@ -85,6 +87,8 @@ final class Worker
             await($resultReaderCoro);
         } catch (\Cancellation|\Async\ThreadChannelException) {
             error_log('[Thrun Worker] Result reader \Cancellation or ThreadChannelException');
+            $cls = get_class($e);
+            error_log("{$cls} {$e->getMessage()} {$e->getTraceAsString()}");
         } catch (\Throwable $e) {
             error_log('[Thrun Worker] Result reader coroutine error: '.$e::class.': '.$e->getMessage());
             if ($abnormalError === null) {
